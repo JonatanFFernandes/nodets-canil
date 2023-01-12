1876c6609fd58de3fa879e6e64b776abc480b2c9
Jonatan Fernandes <jonatan.fernandes@cigamgestor.com.br>
2023-01-12 08:51:04 -0300

    Base criada

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # nodets-canil
+Projeto feito para aprendizagem da linguagem Node JS + TypeScript
+
+### Pré-requisitos globais:
+`npm i -g nodemon typescript ts-node`
+
+### Instalação
+`npm install`
+
+### Para rodar o projeto
+`npm run start-dev`
---
+++ src/server.ts
@@ -0,0 +1,29 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import mustache from 'mustache-express';
+import path from 'path';
+import mainRoutes from './routes/index';
+
+dotenv.config();
+
+//Criação do servidor
+const server = express();
+
+//Configuração do Template Engine
+server.set('view engine', 'mustache');
+server.set('views', path.join(__dirname, 'views'));
+server.engine('mustache', mustache());
+
+//Configuração da Pasta Pública
+server.use(express.static(path.join(__dirname, '../public')));
+
+//Configuração das Rotas
+server.use(mainRoutes);
+
+server.use((req, res) => {
+    res.send('Pagina não encontrada!');
+});
+
+//Colocar o servidor para rodar
+server.listen(process.env.PORT);
+
